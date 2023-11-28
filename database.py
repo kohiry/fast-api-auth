cd6b27a657a6f43644efaede32435f6039a915ad
Max <kohiry@mail.ru>
2023-11-28 20:17:29 +0400

    feat: create req

--- database.py
+++ database.py
@@ -4,6 +4,6 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABSE_URL = 'sqlite:///./todosapp.db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(SQLALCHEMY_DATABSE_URL , connect_args={'check_same_thread': False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
