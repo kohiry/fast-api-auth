cffb15e5ac651f07a7dd7d4fff0a7ca9bd74d5d5
Max <kohiry@mail.ru>
2023-11-30 12:40:57 +0400

    feat: авторизация сделана

--- database.py
+++ database.py
@@ -7,3 +7,10 @@ SQLALCHEMY_DATABSE_URL = 'sqlite:///./todosapp.db'
 engine = create_engine(SQLALCHEMY_DATABSE_URL , connect_args={'check_same_thread': False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
